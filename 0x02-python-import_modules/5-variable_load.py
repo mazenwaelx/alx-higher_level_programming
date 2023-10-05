5c1d7a5769cab1544ebe10d8fac3f4c99ba756a1
mazenwaelx <mazenwael575@gmail.com>
2023-10-05 16:22:03 -0700

    lol

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import variable_load_5
-    
-    word = dir(variable_load_5)
-    
-    for name in word:
-        if name != 'a':
-            continue
-        else:
-            print(name)
+    from variable_load_5 import a
+
+    print(a)
