bc19730aa9472174c9a7800e2ef8d361f460ea9b
mazenwaelx <mazenwael575@gmail.com>
2023-10-08 13:29:59 -0700

    lol

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    for i in range(len(sentence)):
-        count = count + 1
-    return count , sentence[0]
-
+    my_tuple = ()
+    if len(sentence) == 0:
+        my_tuple = 0, "None"
+    else:
+        my_tuple = len(sentence), sentence[0]
+    return my_tuple
