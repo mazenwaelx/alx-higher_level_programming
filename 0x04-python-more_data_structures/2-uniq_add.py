44e5ace13afddf0a5283d10897ab2f8d64407726
mazenwaelx <mazenwael575@gmail.com>
2023-10-11 11:34:48 -0700

    lol

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+ef uniq_add(my_list=[]):
+    new_list = []
+    sum = 0
+    for element in my_list:
+        if element not in my_list
+        sum += element
+        new_list.append(element)
+    return new_list
