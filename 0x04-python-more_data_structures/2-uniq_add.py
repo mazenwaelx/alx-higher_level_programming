b93d1561654191ed16993ca2199a0692421c61e3
mazenwaelx <mazenwael575@gmail.com>
2023-10-11 11:37:15 -0700

    lol

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -5,7 +5,7 @@ ef uniq_add(my_list=[]):
     new_list = []
     sum = 0
     for element in my_list:
-        if element not in new_list
-        sum += element
-        new_list.append(element)
+        if element not in new_list:
+            sum += element
+            new_list.append(element)
     return sum
